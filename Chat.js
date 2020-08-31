6eb90a32f1c4b713c8af8ab8a2e00afe26f299b7
Kevin Nacario <hitchcliff1@gmail.com>
2020-08-31 18:52:03 +0800

    updated css and js icons when clicked

--- Chat.js
+++ Chat.js
@@ -1,9 +1,10 @@
 class InteractiveChatbox {
-    constructor(a, b) {
+    constructor(a, b, c) {
         this.args = {
             button: a,
             chatbox: b
         }
+        this.icons = c;
         this.state = false; 
     }
 
@@ -15,14 +16,27 @@ class InteractiveChatbox {
 
     toggleState(chatbox) {
         this.state = !this.state;
-        this.showOrHide(chatbox);
+        this.showOrHideChatBox(chatbox, this.args.button);
     }
 
-    showOrHide(chatbox) {
+    showOrHideChatBox(chatbox, button) {
         if(this.state) {
             chatbox.classList.add('chatbox--active')
+            this.toggleIcon(true, button);
         } else if (!this.state) {
             chatbox.classList.remove('chatbox--active')
+            this.toggleIcon(false, button);
+        }
+    }
+
+    toggleIcon(state, button) {
+        const { isClicked, isNotClicked } = this.icons;
+        let b = button.children[0].innerHTML;
+
+        if(state) {
+            button.children[0].innerHTML = isClicked; 
+        } else if(!state) {
+            button.children[0].innerHTML = isNotClicked;
         }
     }
 }
